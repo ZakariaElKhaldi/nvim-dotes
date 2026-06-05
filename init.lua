5559e3946e267f251fada67b4a8f6fbbe0278a22
ZakariaElKhaldi <zkariaelkhaldi@gmail.com>
2026-06-05 23:53:17 +0100

    feat: mini + telescope

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
+vim.g.mapleader = " "
+
 vim.pack.add({
-	'https://github.com/nvim-telescope/telescope.nvim.git',
 	'https://github.com/neovim/nvim-lspconfig.git',
 	'https://github.com/mason-org/mason.nvim.git',
 	--theme
@@ -7,12 +8,35 @@ vim.pack.add({
 	'https://github.com/tjdevries/colorbuddy.nvim.git',
 	'https://github.com/datsfilipe/vesper.nvim.git',
 	'https://github.com/ayu-theme/ayu-vim.git',
+
+	'https://github.com/nvim-mini/mini.nvim',
+	'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+
+	'https://github.com/nvim-lua/plenary.nvim',
+	'https://github.com/nvim-telescope/telescope.nvim',
+	'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
 })
 
-vim.g.mapleader = " "
+require('telescope').setup{}
+require('mini.files').setup()
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.keymap.set("n", "<leader><leader>", function()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+	MiniFiles.reveal_cwd()
+end, { desc = "Toggle into currently opened file" })
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 --vim.cmd.colorscheme('srcery')
 --vim.cmd.colorscheme("colorbuddy")
@@ -75,10 +99,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 vim.diagnostic.enable()
 vim.diagnostic.config({
-  float = {
-    border = "rounded",
-  },
-  virtual_text = {
-    severity = vim.diagnostic.severity.ERROR,
-  },})
+	float = {
+		border = "rounded",
+	},
+	virtual_text = {
+		severity = vim.diagnostic.severity.ERROR,
+	},
+})
 vim.keymap.set('n', '<leader>w', vim.diagnostic.open_float)
+
+if vim.g.neovide then
+	vim.g.neovide_scale_factor = 1.0
+
+	vim.keymap.set("n", "<C-=>", function()
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+	end)
+
+	vim.keymap.set("n", "<C-->", function()
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+	end)
+
+	vim.keymap.set("n", "<C-0>", function()
+		vim.g.neovide_scale_factor = 1.0
+	end)
+end
